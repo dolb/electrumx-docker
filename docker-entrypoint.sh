b45d3dec8e1b724c70ef1e69ae13b8cdea10250d
dolb <dolb1987@gmail.com>
2018-02-08 19:38:17 +0100

    Increased electrum server initial bootup time by several times. Removing fullnode recursive chown from the ENTRYPOINT did the trick.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-chown -R zcluser /home/zcluser
+ZCLUSER_HOME="/home/zcluser/"
+PATHS=(".zclassic" ".zcash-params" "zcl_electrum_db" "certs")
+
+chown zcluser /home/zcluser
+
+for p in ${PATHS[*]}; do
+  [[ -d  "$PTH" ]] && chown zcluser $PTH
+done
 
 if [ "$CUSTOM_USER" = "zcluser" ]; then
     sudo -H -u $CUSTOM_USER -E /bin/bash -c "$@"
